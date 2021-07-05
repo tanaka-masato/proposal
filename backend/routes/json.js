e99cae0bdf19827ccb06b4b4da006543767d3530
tanaka-masato <tnkmasato09031999@gmail.com>
2021-07-06 01:07:34 +0900

    計算の実装

--- backend/routes/json.js
+++ backend/routes/json.js
@@ -1,16 +1,19 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
 
 /* サンプルAPI① 
  * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
  * JSON形式で文字列を返す。
  */
-router.get('/retort', function(req, res, next) {
+router.get('/retort', function (req, res, next) {
   const fs = require('fs');
-  const json = JSON.parse(fs.readFileSync('./json/retort.json','utf8'));
+  const json = JSON.parse(fs.readFileSync('./json/retort.json', 'utf8'));
   res.header('Content-Type', 'application/json; charset=utf-8')
   // res.send(param);
-  res.json({json});
+  res.json({
+    json
+  });
   console.log(json);
 });
 
@@ -18,24 +21,75 @@ router.get('/retort', function(req, res, next) {
  * http://localhost:3000/samples/hello にGETメソッドのリクエストを投げると、
  * JSON形式で文字列を返す。
  */
-router.get('/hello', function(req, res, next) {
-  var param = {"result":"Hello World !"};
+router.get('/hello', function (req, res, next) {
+  var param = {
+    "result": "Hello World !"
+  };
   res.header('Content-Type', 'application/json; charset=utf-8')
   res.send(param);
 });
 
-router.post('/retort', function(req, res, next) {
+router.post('/retort', function (req, res, next) {
   res.header('Content-Type', 'application/json; charset=utf-8')
   console.log("post");
   console.log(req.body);
+  console.log(req.body.height)
+
+
+  let desirable_weight = desirable_weight_calculation(req.body.height);
+  console.log(desirable_weight)
+
+
+  let bmr = bmr_calculation(req.body.height, req.body.weight, req.body.age, req.body.sex);
+  console.log(bmr)
+
+  let tdee = tdee_calculation(bmr, req.body.work_level)
+  console.log(tdee)
+
+  let pfc_balance = pfc_balance_calculation(tdee, desirable_weight, req.body.weight)
+  console.log(pfc_balance)
+  
 });
 
-router.options('/', (req, res, next) => {
-  res.header('Content-Type', 'application/json; charset=utf-8')
-  console.log("options");
-  // console.log(res)
-  console.log(req.body)
-  console.log(res.body)
-});
+// 適正体重の計算
+function desirable_weight_calculation(height) {
+  height /= 100
+  let appropriate_weight = height * height * 22
+  return appropriate_weight
+}
 
+// 基礎代謝の計算
+function bmr_calculation(height, weight, age, sex) {
+  let basal_metabolism_rate = 0
+  if (sex == "男") {
+    basal_metabolism_rate = 13.397 * weight + 4.799 * height - 5.677 * age + 88.362
+  } else if (sex == "女") {
+    basal_metabolism_rate = 9.247 * weight + 3.098 * height - 4.33 * age + 447.593
+  }
+  return basal_metabolism_rate
+}
+
+// １日の総カロリーの計算
+function tdee_calculation(bmr, work_level) {
+  return tdee = bmr * work_level
+}
+
+function pfc_balance_calculation(tdee, desirable_weight, weight) {
+  let pfc_balance = new Array(3)
+
+  if (desirable_weight < weight){
+    tdee -= 500
+    pfc_balance[0] = tdee * 0.2 / 4
+    pfc_balance[1] = tdee * 0.2 / 9
+    pfc_balance[2] = tdee * 0.6 / 4
+  } else{
+    tdee += 500
+    pfc_balance[0] = tdee * 0.2 / 4
+    pfc_balance[1] = tdee * 0.2 / 9
+    pfc_balance[2] = tdee * 0.6 / 4
+  }
+
+
+  return pfc_balance
+}
 module.exports = router;
--- frontend/pages/input.vue
+++ frontend/pages/input.vue
@@ -3,26 +3,40 @@
     <div class="input">
       <div class="height flex">
         <p>身長</p>
-        <input type="text" v-model="test.height" />
+        <input type="text" v-model="personally_information.height" />
       </div>
       <div class="height flex">
         <p>体重</p>
-        <input type="text" v-model="test.weight" />
+        <input type="text" v-model="personally_information.weight" />
       </div>
-      <!-- <div class="height flex">
+      <div class="height flex">
         <p>年齢</p>
-        <input type="text" />
+        <input type="text" v-model="personally_information.age" />
       </div>
       <div class="height flex">
         <p>性別</p>
-        <input type="text" />
+        <select v-model="personally_information.sex" id="">
+          <option>男</option>
+          <option>女</option>
+        </select>
+      </div>
+      <div class="height flex">
+        <p>活動レベル</p>
+        <select v-model="personally_information.work_level" id="">
+          <option :value="1.2">ほぼ運動しない。通勤、デスクワーク程度</option>
+          <option :value="1.375">軽い運動。週に1回〜2回程度の運動</option>
+          <option :value="1.55">中程度の運動。週に3回〜5回程度の運動</option>
+          <option :value="1.725">激しい運動。週に6回〜7回程度の運動</option>
+          <option :value="1.9">非常に激しい運動。１日に2回程度の運動</option>
+        </select>
       </div>
       <div class="height flex">
         <p>地域</p>
-        <input type="text" />
-      </div> -->
+        <input type="text" v-model="personally_information.area" />
+      </div>
     </div>
     <div class="new-submit-btn" @click="submit()">送信</div>
+    {{ resopnse }}
   </main>
 </template>
 
@@ -30,22 +44,26 @@
 export default {
   data() {
     return {
-      test: {
+      personally_information: {
         height: "",
         weight: "",
+        age: "",
+        sex: "",
+        work_level: "",
+        area: "",
       },
+      resopnse: null,
     };
   },
   methods: {
     async submit() {
-      let params = new URLSearchParams();
-      params.append('text', 'テストだよー');
       this.$axios
-        .$post("json/retort", this.test)
+        .$post("/json/retort", this.personally_information)
         .then((resopnse) => {
-          console.log("送信したテキスト：" + resopnse.data.text);
+          console.log("送信したテキスト：" + resopnse);
+          console.log("受け取り");
+          this.resopnse = resopnse;
         });
-      console.log(params);
     },
   },
 };
